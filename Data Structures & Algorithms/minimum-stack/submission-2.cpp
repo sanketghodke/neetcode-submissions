e3893508ccb2de3dd8b97ce3844259162078abf8
sanketghodke <109572200+sanketghodke@users.noreply.github.com>
2026-07-19 20:27:16 +1000

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(!st.size())
+        {
+            st.push({val,val});
+        }
+        else{
+            st.push({val, min(val, st.top().second)});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        
+    }
+    
+    int top() { 
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
