630ea4ba069c82cfd7837a4fb1f192ecf64f1b22
sanketghodke <109572200+sanketghodke@users.noreply.github.com>
2026-07-25 08:04:51 +1000

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void recc(int ind, vector<int>& nums,vector<int> temp, vector<vector<int>>& ans){
+        if(ind == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        temp.push_back(nums[ind]);
+        recc(ind+1, nums, temp, ans);
+
+        temp.pop_back();
+        recc(ind+1, nums, temp, ans);
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        recc(0, nums,temp, ans);
+        return ans;
+    }
+};
